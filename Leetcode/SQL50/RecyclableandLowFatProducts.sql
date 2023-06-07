e0e9739a5b309ac57efbd24566aa78fa6efdc6bb
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2023-06-07 20:55:52 +0600

    Create RecyclableandLowFatProducts.sql

---
+++ Leetcode/SQL50/RecyclableandLowFatProducts.sql
@@ -0,0 +1,7 @@
+--Write an SQL query to find the ids of products that are both low fat and recyclable.
+--Return the result table in any order.
+
+
+--MSSQL
+select product_id from Products
+where low_fats = 'Y' and recyclable = 'Y';
